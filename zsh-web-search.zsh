b0eacc8e3e45f2743aa26517be2968c4d7d90b06
Li Haoyuan <1513624626@qq.com>
2024-08-02 14:58:20 +0800

    docs: Add LICENSE.txt

--- zsh-web-search.zsh
+++ zsh-web-search.zsh
@@ -1,3 +1,21 @@
+#   ZSH Web Search plugin allows users to search the web from ZShell in the terminal
+#   Copyright (C) 2024  Haoyuan Li
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 ## Add custom search engine here
 declare -A search_url_arr=(
   ["google"]="https://google.com/search?q="
